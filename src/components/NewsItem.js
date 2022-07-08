e1751a4a2b24bdbadd1b95777833fcf3d8325098
Istaranjan Das <istaranjandas@gmail.com>
2022-07-08 19:43:52 +0530

    added components and structure + API call

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
     
   render() {
-    let {title,description} = this.props;
+    let {title,description,imageUrl,newsUrl} = this.props;
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-            <img className="card-img-top" src="https://i.ytimg.com/vi/WB4o2U6yEho/maxresdefault.jpg" alt="..."/>
+            <img className="card-img-top" src={imageUrl} alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href="/" className="btn btn-primary">Go somewhere</a>
+                <a href={newsUrl} className="btn btn-primary">Read more</a>
             </div>
         </div>
       </div>
